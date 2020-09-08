ea73e5d17c475ab8cf7bbf62d7abcfec1888f30d
Dominik Szarkiel <d.szarkiel@gmail.com>
2020-09-08 21:01:42 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,4 @@
 def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    return "false"
+  if board[index] != " " || board[index] != "" || board[index] != nil
   end
 end
